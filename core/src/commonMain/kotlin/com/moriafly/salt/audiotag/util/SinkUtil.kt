dcf2c29a639534d63a4026f4bd99aa8b187732f1
不要糖醋放椒盐 <moriafly@163.com>
2025-03-31 14:12:25 +0800

    Update SinkUtil.kt

--- core/src/commonMain/kotlin/com/moriafly/salt/audiotag/util/SinkUtil.kt
+++ core/src/commonMain/kotlin/com/moriafly/salt/audiotag/util/SinkUtil.kt
@@ -22,7 +22,7 @@ import kotlinx.io.Sink
 /**
  * Writes three bytes containing [int], in the big-endian order, to this sink.
  */
-fun Sink.writeInt24(int: Int) {
+internal fun Sink.writeInt24(int: Int) {
     require(int in 0..0xFFFFFF) { "The int must fit in 24 bits (0..16777215)." }
     writeByte(((int ushr 16) and 0xFF).toByte())
     writeByte(((int ushr 8) and 0xFF).toByte())
